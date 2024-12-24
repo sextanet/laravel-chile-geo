7706ca211d683fa6498721c2565cf1d27b0196dd
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2024-12-24 20:43:34 -0300

    feat: seeder with first config file

--- config/chile-geo.php
+++ config/chile-geo.php
@@ -1,6 +1,9 @@
 <?php
 
-// config for SextaNet/LaravelChileGeo
 return [
+    'chile_geo_country_id' => env('CHILE_GEO_COUNTRY_ID', 1),
 
+    'table_regions' => env('CHILE_GEO_TABLE_REGIONS', 'regions'),
+
+    'table_communes' => env('CHILE_GEO_TABLE_COMMUNES', 'communes'),
 ];
